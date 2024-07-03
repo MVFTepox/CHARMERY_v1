eeb9f49d699ce2dbb746b6b1beeb2a2039592800
Mario Fraire <fraire.tepox.mario.valentin@gmail.com>
2024-07-03 08:06:38 -0600

    inicio

---
+++ CHARMERY-V1/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hola mundo</h1>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style> 
+  h1 {
+    color: red;
+    font-size: 50px;
+  }
+</style>
